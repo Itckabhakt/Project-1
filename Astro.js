a061018d677674ac8455f3cc54ba729a2b8d95e1
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-06-19 00:09:31 +0530

    Update Astro.js

--- Astro.js
+++ Astro.js
@@ -1,10 +1,1 @@
-const channelData = {
-  "Astro_plus2": {
-    "file": "https://linear05-playback.sooka.my/CH2/masterCH2.mpd",
-    "drm": {
-      "clearkey": {
-        "keyId": "4a267ba09b3a43969364ee77f76dd622",
-        "key": "5b00d4341e90b3022b5e57139ae8ab16"
-      }
-    }
-  }
+
