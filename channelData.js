6292ac03eb1bb66b2ec395fc01b0182338c7777a
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-06-19 19:34:18 +0530

    Update channelData.js

--- channelData.js
+++ channelData.js
@@ -1,1 +1,173 @@
-const _0x1d0950=_0x455d;function _0x1a41(){const _0x5d155d=['skycdn.it/','2050HiNVfm','-dash1-prd','4140969483','80HTLAnH','13916c5bfe','skycdp.com','36snbNcA','96e498b70e','e7c650ac72','22ca2e250b','254670TxHZEm','https://ma','598f5e4f10','8f84185910','d4e0a78e36','9d76d25b5b','nearjitp-p','tvAssetId=','0036e5d0e8','-cdn.rikst','ashiso/514','9d4f43910a','6401e1fead','729982c972','4207753iMUrjn','41c58b2ecb','016a/31917','n.dmdsdp.c','0bc653d830','99df778055','dcd0311411','nvidia_ct_','ort251/mas','4349248BSoVgt','93465PKXBOy','ve/eds/la1','d17169f0f8','sport/play','/016a/3120','fest.mpd','37ac314d79','70CNpQIL','/master.mp','_Scrambled','1-obc1112-','1122334455','fff8cbf903','9f3ed7b413','485521DAvcEt','837dceb13b','om/dash/SV','near302-it','list_cbcs.','atel.tv/li','2-obc1112-','9/FHD/skys','/45371d5a/','1f38af550a','v/1/11/das','h/TV2_HD/m','f345828d9c','e18ae0ca8a','mpd','45004625a0','7757d3fa8c','tro.com.my','09212/mani','637923bd4a','6677889900','1&adap=no_','09e0ff3f7e','s-appeartv','https://da','d02.cdn.av','d4e040d255','t_009/zdf-','rikstv_571','dolby&Riks','4b202df114','portseriea','1-prod.aka','near311-it','https://wp','https://aw','00368000ea','8dfb087f17','79478cadc4','ter.mpd','/FHD/skysp','09091/mani','live-ch-pr','maized.net','488919VdZAvY','_High/la1.','/dash-wv/d','5d9937d586','-ll.cdn13.','bfb0134de6','s1-live-00','ea8fa43cc7','d?version=','91da6150ce','2ryhZfu','layback.as','aad360f46d','https://li','https://p7','sh2.antik.','-default.m','102e481cd4','832a8271bb','/MPEG-DASH','.selector.','od.prod.cd','ecbeeafd84','8/default.','13095ChxFht','dab673a2dd','c05b3b8243','62c4bde44a','0b5e6af8d1','sk/stream/','anifest.mp','025915bd30','de-hd/cenc','v.no/rikst','eecfc0164e'];_0x1a41=function(){return _0x5d155d;};return _0x1a41();}(function(_0x19574b,_0x576043){const _0x50f7ba=_0x455d,_0xbfab31=_0x19574b();while(!![]){try{const _0x38d9ab=parseInt(_0x50f7ba(0xaa))/(0x10c9+0x2177*-0x1+0x10af)*(-parseInt(_0x50f7ba(0xe0))/(0x8*-0x39d+-0xf58+-0x2c42*-0x1))+-parseInt(_0x50f7ba(0xd6))/(-0x1*-0x199e+-0xd55*-0x1+-0x38*0xb2)+parseInt(_0x50f7ba(0xfd))/(0x1*-0x2ab+0x7ea+-0x1*0x53b)*(-parseInt(_0x50f7ba(0x11c))/(0xb3e+0x6c3+-0x11fc))+-parseInt(_0x50f7ba(0x104))/(-0x1d9+-0x2dc+0xad*0x7)*(-parseInt(_0x50f7ba(0x123))/(-0xb83+-0x43a*0x4+-0x1*-0x1c72))+-parseInt(_0x50f7ba(0x11b))/(-0x5*-0x4dd+-0x17cd+0x2*-0x3e)+-parseInt(_0x50f7ba(0xee))/(0xad1*-0x1+-0xf66+0x1a40)*(-parseInt(_0x50f7ba(0xfa))/(-0x12a1+-0x2c8+0x1573*0x1))+parseInt(_0x50f7ba(0x112))/(-0x2616+-0x258d+0x4bae)*(parseInt(_0x50f7ba(0x100))/(0x215*-0x10+-0x46e+0x25ca));if(_0x38d9ab===_0x576043)break;else _0xbfab31['push'](_0xbfab31['shift']());}catch(_0x5176e0){_0xbfab31['push'](_0xbfab31['shift']());}}}(_0x1a41,-0x1*-0x5519e+0x4b759+-0x5645b*0x1));function _0x455d(_0x8d5128,_0x37bcec){const _0x3dcda9=_0x1a41();return _0x455d=function(_0x1ce17e,_0x15a771){_0x1ce17e=_0x1ce17e-(0xb0+-0x33b*-0x7+-0x16a6);let _0x9a65a9=_0x3dcda9[_0x1ce17e];return _0x9a65a9;},_0x455d(_0x8d5128,_0x37bcec);}const channelData={'BBC1':{'file':_0x1d0950(0xcc)+_0x1d0950(0x126)+_0x1d0950(0xd4)+_0x1d0950(0xeb)+_0x1d0950(0x115)+_0x1d0950(0xac)+_0x1d0950(0xd3)+_0x1d0950(0x121),'drm':{'clearkey':{'keyId':_0x1d0950(0x110)+_0x1d0950(0x10f)+_0x1d0950(0xe2)+'d0','key':_0x1d0950(0x106)+_0x1d0950(0xc4)+_0x1d0950(0xd0)+'ee'}}},'TVLA1':{'file':_0x1d0950(0x105)+_0x1d0950(0xc3)+_0x1d0950(0xaf)+_0x1d0950(0x11d)+_0x1d0950(0xe9)+_0x1d0950(0x125)+_0x1d0950(0xd7)+_0x1d0950(0xb8),'drm':{'clearkey':{'keyId':_0x1d0950(0xf2)+_0x1d0950(0xb6)+_0x1d0950(0xa8)+'ee','key':_0x1d0950(0x117)+_0x1d0950(0xb3)+_0x1d0950(0xbd)+'31'}}},'SKY_251_IT':{'file':_0x1d0950(0xe3)+_0x1d0950(0xcb)+_0x1d0950(0xfb)+_0x1d0950(0xea)+_0x1d0950(0xf9)+_0x1d0950(0x114)+_0x1d0950(0xd2)+_0x1d0950(0x11a)+_0x1d0950(0xd1),'drm':{'clearkey':{'keyId':_0x1d0950(0xce)+_0x1d0950(0xec)+_0x1d0950(0xf0)+'af','key':_0x1d0950(0x107)+_0x1d0950(0xb7)+_0x1d0950(0xb9)+'00'}}},'ITV1_GB':{'file':_0x1d0950(0xcc)+_0x1d0950(0xb0)+_0x1d0950(0xd4)+_0x1d0950(0xeb)+_0x1d0950(0x115)+_0x1d0950(0xac)+_0x1d0950(0xbc)+_0x1d0950(0x121),'drm':{'clearkey':{'keyId':_0x1d0950(0xd9)+_0x1d0950(0xfc)+_0x1d0950(0x122)+'c1','key':_0x1d0950(0x103)+_0x1d0950(0x111)+_0x1d0950(0xc0)+'9b'}}},'ZDF_DE':{'file':_0x1d0950(0xe4)+_0x1d0950(0xdc)+_0x1d0950(0xca)+_0x1d0950(0xd5)+_0x1d0950(0xb2)+_0x1d0950(0xc5)+_0x1d0950(0xf6)+_0x1d0950(0xe6)+'pd','drm':{'clearkey':{'keyId':_0x1d0950(0xba)+_0x1d0950(0xef)+_0x1d0950(0xa9)+'60','key':_0x1d0950(0x101)+_0x1d0950(0xf8)+_0x1d0950(0x108)+'f3'}}},'CTSPORTS_CZ':{'file':_0x1d0950(0xc2)+_0x1d0950(0xe5)+_0x1d0950(0xf3)+_0x1d0950(0x119)+_0x1d0950(0x11f)+_0x1d0950(0xae)+_0x1d0950(0xb8),'drm':{'clearkey':{'keyId':_0x1d0950(0xa7)+_0x1d0950(0xbe)+_0x1d0950(0xa7)+'66','key':_0x1d0950(0xa7)+_0x1d0950(0xbe)+_0x1d0950(0xa7)+'66'}}},'TV2_Direkte_NO':{'file':_0x1d0950(0xcd)+_0x1d0950(0xc1)+_0x1d0950(0x10d)+_0x1d0950(0xf7)+_0x1d0950(0xb4)+_0x1d0950(0xb5)+_0x1d0950(0xf4)+_0x1d0950(0xde)+_0x1d0950(0xbf)+_0x1d0950(0xc7)+_0x1d0950(0x10b)+_0x1d0950(0xc6)+'1','drm':{'clearkey':{'keyId':_0x1d0950(0xdb)+_0x1d0950(0x109)+_0x1d0950(0xc8)+'31','key':_0x1d0950(0xf5)+_0x1d0950(0xf1)+_0x1d0950(0xfe)+'fe'}}},'Sky_Sports_Calcio_IT':{'file':_0x1d0950(0xe3)+_0x1d0950(0xad)+_0x1d0950(0xfb)+_0x1d0950(0xda)+_0x1d0950(0xff)+_0x1d0950(0x120)+_0x1d0950(0xb1)+_0x1d0950(0xc9)+_0x1d0950(0x124)+'d','drm':{'clearkey':{'keyId':_0x1d0950(0x10c)+_0x1d0950(0xcf)+_0x1d0950(0xdf)+'d9','key':_0x1d0950(0x116)+_0x1d0950(0xab)+_0x1d0950(0x11e)+'e2'}}},'Spotv':{'file':_0x1d0950(0xe3)+_0x1d0950(0x10a)+_0x1d0950(0xe1)+_0x1d0950(0xbb)+_0x1d0950(0xd8)+_0x1d0950(0x10e)+_0x1d0950(0xed)+_0x1d0950(0xb8),'drm':{'clearkey':{'keyId':_0x1d0950(0x102)+_0x1d0950(0x118)+_0x1d0950(0xe8)+'10','key':_0x1d0950(0xdd)+_0x1d0950(0xe7)+_0x1d0950(0x113)+'08'}}}};
+const channelData = {
+ "BBC1": {
+    "file": "https://wp1-obc1112-live-ch-prod.prod.cdn.dmdsdp.com/dash/SV09091/manifest.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "6401e1fead9d4f43910aaad360f46dd0",
+        "key": "598f5e4f10d4e040d25579478cadc4ee"
+      }
+    }
+  },
+  "TVLA1": {
+    "file": "https://mad02.cdn.avatel.tv/live/eds/la1/MPEG-DASH_Scrambled_High/la1.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "0b5e6af8d1f345828d9cfff8cbf903ee",
+        "key": "99df7780551f38af550a637923bd4a31"
+      }
+    }
+  },
+  "SKY_251_IT": {
+    "file": "https://linear311-it-dash1-prd.selector.skycdn.it/016a/31917/FHD/skysport251/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "00368000eaecbeeafd84c05b3b8243af",
+        "key": "8f84185910e18ae0ca8a45004625a000"
+      }
+    }
+  },
+  "ITV1_GB": {
+    "file": "https://wp2-obc1112-live-ch-prod.prod.cdn.dmdsdp.com/dash/SV09212/manifest.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "5d9937d586414096948337ac314d79c1",
+        "key": "22ca2e250b729982c97209e0ff3f7e9b"
+      }
+    }
+  },
+  "ZDF_DE": {
+    "file": "https://p7s1-live-001-prod.akamaized.net/45371d5a/t_009/zdf-de-hd/cenc-default.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "7757d3fa8cdab673a2dd9f3ed7b41360",
+        "key": "96e498b70eeecfc0164ed4e0a78e36f3"
+      }
+    }
+  },
+  "CTSPORTS_CZ": {
+    "file": "https://dash2.antik.sk/stream/nvidia_ct_sport/playlist_cbcs.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "11223344556677889900112233445566",
+        "key": "11223344556677889900112233445566"
+      }
+    }
+  },
+"TV2_Direkte_NO": {
+    "file": "https://aws-appeartv-cdn.rikstv.no/rikstv/1/11/dash/TV2_HD/manifest.mpd?version=1&adap=no_dolby&RikstvAssetId=rikstv_5711",
+    "drm": {
+      "clearkey": {
+        "keyId": "bfb0134de69d76d25b5b4b202df11431",
+        "key": "025915bd3062c4bde44a13916c5bfefe"
+      }
+    }
+  },
+"Sky_Sports_Calcio_IT": {
+    "file": "https://linear302-it-dash1-prd-ll.cdn13.skycdp.com/016a/31209/FHD/skysportseriea/master.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "0036e5d0e88dfb087f1791da6150ced9",
+        "key": "0bc653d830837dceb13bd17169f0f8e2"
+      }
+    }
+  },
+ "Spotv": {
+    "file": "https://linearjitp-playback.astro.com.my/dash-wv/dashiso/5148/default.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "e7c650ac72dcd0311411832a8271bb10",
+        "key": "ea8fa43cc7102e481cd441c58b2ecb08"
+      }
+    }
+  },
+   "Prime1": {
+    "file": "https://aba5sdmaaaaaaaamkycwliah6buj4.otte.live.cf.ww.aiv-cdn.net/pdx-nitro/live/clients/dash/enc/tgcrfjcgia/out/v1/7296f026409c4e9ba8844a238628f9ad/cenc.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "d1badeadb11f16bbd0ad7d47914d939e",
+        "key": "bd52d185d3537c4c1759db6d66f90f31"
+      }
+    }
+  },
+   "Prime2": {
+    "file": "https://aba5sdmaaaaaaaamcs7bgwx2cgzhi.otte.live.cf.ww.aiv-cdn.net/pdx-nitro/live/clients/dash/enc/bopqv4fykb/out/v1/01d1290c69584c7cbf58b8ab404ea1b8/cenc.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "6da8c268d58a0fe4ff75d297359a8ad7",
+        "key": "eb0ae4c2c24a52070154499bb09a36cb"
+      }
+    }
+  },
+   "Prime3": {
+    "file": "https://aba5sdmaaaaaaaamibyasmmfoav3v.otte.live.cf.ww.aiv-cdn.net/pdx-nitro/live/clients/dash/enc/dmmqe7ssji/out/v1/3775f3d149f34a7a81d8cd7d58118e40/cenc.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "1105b85e6db6214e686e462206ed86ec",
+        "key": "59d851187354b65fa2d14829046893ef"
+      }
+    }
+  },
+   "Astro": {
+    "file": "https://linearjitp-playback.astro.com.my/dash-wv/linear/2504/default.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "03c2e0af2f8159f9f0ce9b5dbc865f10",
+        "key": "cd84ed136b0cc71f8ab8cd4d4f6a2e4c"
+      }
+    }
+  },
+   "Astro_plus": {
+    "file": "https://linear05-playback.sooka.my/CH2/masterCH2.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "4a267ba09b3a43969364ee77f76dd622",
+        "key": "5b00d4341e90b3022b5e57139ae8ab16"
+      }
+    }
+  },
+   "Willow": {
+    "file": "https://linear-novi.stvacdn.spectrum.com/LIVE/5248/bpk-tv/10364/drm/manifest.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "475d7932b5f352bb92016b75b60c58fa",
+        "key": "ea37faa137ae8f644105c4fcc189a35f"
+      }
+    }
+  },
+   "Skycricket(ukvpn)": {
+    "file": "https://linear007-gb-dash1-prd-ak.cdn.skycdp.com/016a/Content/DASH_003_720_120/Live/channel(skysportscricket)/manifest_720-120.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "00056d5c8882917a4463d5b42ba86c31",
+        "key": "226c39dd7d41b6f0e27cb7d8940f644f"
+      }
+    }
+  },
+   "Skyaction(ukvpn)": {
+    "file": "https://linear013-gb-dash1-prd-ak.cdn.skycdp.com/016a/Content/DASH_003_720_120/Live/channel(skysportsaction)/manifest_720.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "0003d0fbdf31675b39ee3ea8b9f0ce68",
+        "key": "ebfa3109af67dc34a10321f78627b631"
+      }
+    }
+  },
+  "criclifemax(gulfcountries": {
+    "file": "https://cmaxlive.starzplayarabia.com/out/v1/bebec73ea17b40b3bd0257ad8e97b531/index.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "ec2168a420478062ec89540dad862bdd",
+        "key": "d6d63d7a1391d2a89fb12e5e1c5ced01"
+      }
+    }
+  },
+  "Ptvsports(pakvpn)": {
+    "file": "https://live4.shoq.com.pk/live/eds/PTV_Sports/DASH/PTV_Sports.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "9ecad6c4413f8bdc54712ce6c072a2cf",
+        "key": "442df559c369bdada8ba3abe97811575"
+      }
+    }
+  },
+ };
