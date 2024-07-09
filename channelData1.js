5e7eb93d4a001058e7290689f053abcc2ba2fcd0
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-07-09 23:30:02 +0530

    Create channelData1.js

---
+++ channelData1.js
@@ -0,0 +1,20 @@
+const channelData = {
+  "A": {
+    "file": "https://live.vrtcdn.be/groupc/live/8edf3bdf-7db3-41c3-a318-72cb7f82de66/live.isml/.mpd?filter=%28%21%28type%3D%3D%22audio%22%26%26FourCC%21%3D%22AACL%22%29%29",
+    "drm": {
+      "clearkey": {
+        "keyId": "ef4daa3c90333621e4115ff1ca5bfbda",
+        "key": "424c9902fce5a2e7dcbeb37ffc06b906"
+      }
+    }
+  },
+  "no": {
+    "file": "https://linear301-it-dash1-prd-akg0.cdn13.skycdp.com/016a/31023/FHD/skysportuno/master_stereo.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "0036dc70e4d659fe5ae57b52793a1ff3",
+        "key": "55517f806b6be917c2bf0e9cf9acc44f"
+      }
+    }
+  }
+};
