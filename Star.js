82a370cb0817beb4170d4a92e0b4e66a2fdd7dcd
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-07-09 17:10:27 +0530

    Update Star.js

--- Star.js
+++ Star.js
@@ -3,8 +3,8 @@ const channelData = {
     "file": "https://bpprod3catchup.akamaized.net/bpk-tv/irdeto_com_Channel_252/output/xxx.mpd",
     "drm": {
       "clearkey": {
-        "keyId": "0de75732070957ac97f5264807e7c5f5",
-        "key": "b2fa6ef41fd84015b46282ab4bd6a59a"
+        "keyId": "abe6e051aa78580b8f2bd7bff4ef5dfb",
+        "key": "6e74606a09e30ad2ec635b9dfa46fea9"
       }
     }
   },
@@ -12,12 +12,12 @@ const channelData = {
     "file": "https://bpprod2catchup.akamaized.net/bpk-tv/irdeto_com_Channel_250/output/xxx.mpd",
     "drm": {
       "clearkey": {
-        "keyId": "95051a8f55df5cff813cd603dc3981c4",
-        "key": "5f45dd31b3485d87b8ab4246dbdd7eea"
+        "keyId": "d7bc355536265ef0a5dffd59fe3c153b",
+        "key": "1b649c80e6cb00823ca289a2e81ca27f"
       }
     }
   },
-  "Starenglish2": {
+  "Star2": {
     "file": "https://bpprod5catchup.akamaized.net/bpk-tv/irdeto_com_Channel_251/output/xxx.mpd",
     "drm": {
       "clearkey": {
