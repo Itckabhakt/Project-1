87b0c01a01abd4584e910e7d63314954e99289a3
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-07-13 14:25:23 +0530

    Update TntSportuk.js

--- TntSportuk.js
+++ TntSportuk.js
@@ -1,37 +1,37 @@
 const channelData = {
   "1": {
-    "file": "https://linear001-ie-dash1-prd-ak.cdn.skycdp.com/016a/Content/DASH_003_720_120/Live/channel(tntsport1)/manifest_720.mpd",
+    "file": "https://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/clients/dash/enc/wf8usag51e/out/v1/bd3b0c314fff4bb1ab4693358f3cd2d3/cenc.mpd",
     "drm": {
       "clearkey": {
-        "keyId": "00051ad8db73a944abe9ec83ad88987b",
-        "key": "100c985696718d923c4b1289c1cf0d7d"
+        "keyId": "ae26845bd33038a9c0774a0981007294",
+        "key": "63ac662dde310cfb4cc6f9b43b34196d"
       }
     }
   },
-  "Star1": {
-    "file": "https://bpprod2catchup.akamaized.net/bpk-tv/irdeto_com_Channel_250/output/xxx.mpd",
+  "2": {
+    "file": "https://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/clients/dash/enc/f0qvkrra8j/out/v1/f8fa17f087564f51aa4d5c700be43ec4/cenc.mpd",
     "drm": {
       "clearkey": {
-        "keyId": "0ab3dfd82cf15467ac684aebf75ee0a9",
-        "key": "de3194bce7f3123cbd02809d807325c6"
+        "keyId": "6d1708b185c6c4d7b37600520c7cc93c",
+        "key": "1aace05f58d8edef9697fd52cb09f441"
       }
     }
   },
-  "Star2": {
-    "file": "https://bpprod5catchup.akamaized.net/bpk-tv/irdeto_com_Channel_251/output/xxx.mpd",
+  "3": {
+    "file": "https://ottb.live.cf.ww.aivhttps://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/clients/dash/enc/lsdasbvglv/out/v1/bb548a3626cd4708afbb94a58d71dce9/cenc.mpd",
     "drm": {
       "clearkey": {
-        "keyId": "f4bd4e712cad58ee8762d9f294c13647",
-        "key": "99df910ef7250a42d0c82145ca1b12a3"
+        "keyId": "4e993aa8c1f295f8b94e8e9e6f6d0bfe",
+        "key": "86a1ed6e96caab8eb1009fe530d2cf4f"
       }
     }
   },
-  "Star2": {
-    "file": "https://bpprod5catchup.akamaized.net/bpk-tv/irdeto_com_Channel_251/output/xxx.mpd",
+  "4": {
+    "file": "https://ottb.live.cf.ww.aiv-cdn.net/lhr-nitro/live/clients/dash/enc/i2pcjr4pe5/out/v1/912e9db56d75403b8a9ac0a719110f36/cenc.mpd",
     "drm": {
       "clearkey": {
-        "keyId": "f4bd4e712cad58ee8762d9f294c13647",
-        "key": "99df910ef7250a42d0c82145ca1b12a3"
+        "keyId": "e31a5a81caff5d07ea2411a571fc2e59",
+        "key": "96c5ef69479732ae734f962748c19729"
       }
     }
   }
