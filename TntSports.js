90dfcc360628b22f958309abacfebd735a88ee84
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-07-13 14:19:59 +0530

    Create TntSports.js

---
+++ TntSports.js
@@ -0,0 +1,38 @@
+const channelData = {
+  "1": {
+    "file": "https://linear001-ie-dash1-prd-ak.cdn.skycdp.com/016a/Content/DASH_003_720_120/Live/channel(tntsport1)/manifest_720.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "00051ad8db73a944abe9ec83ad88987b",
+        "key": "100c985696718d923c4b1289c1cf0d7d"
+      }
+    }
+  },
+  "Star1": {
+    "file": "https://bpprod2catchup.akamaized.net/bpk-tv/irdeto_com_Channel_250/output/xxx.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "0ab3dfd82cf15467ac684aebf75ee0a9",
+        "key": "de3194bce7f3123cbd02809d807325c6"
+      }
+    }
+  },
+  "Star2": {
+    "file": "https://bpprod5catchup.akamaized.net/bpk-tv/irdeto_com_Channel_251/output/xxx.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "f4bd4e712cad58ee8762d9f294c13647",
+        "key": "99df910ef7250a42d0c82145ca1b12a3"
+      }
+    }
+  },
+  "Star2": {
+    "file": "https://bpprod5catchup.akamaized.net/bpk-tv/irdeto_com_Channel_251/output/xxx.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "f4bd4e712cad58ee8762d9f294c13647",
+        "key": "99df910ef7250a42d0c82145ca1b12a3"
+      }
+    }
+  }
+};
