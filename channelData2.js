96abd4bd0c5c9bb64319dad73e8d3273762b0a54
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-09-07 21:47:26 +0530

    Update channelData2.js

--- channelData2.js
+++ channelData2.js
@@ -25,5 +25,14 @@ const channelData = {
         "key": "442df559c369bdada8ba3abe97811575"
       }
     }
+  },
+  "astro": {
+    "file": "https://d1yws6emo43ny.cloudfront.net/CH5/masterCH5.mpd",
+    "drm": {
+      "clearkey": {
+        "keyId": "03c2e0af2f8159f9f0ce9b5dbc865f10",
+        "key": "cd84ed136b0cc71f8ab8cd4d4f6a2e4c"
+      }
+    }
   }
  };
