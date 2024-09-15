eec33db586b581059a4966d05fb2343ea1e3f973
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-09-15 20:50:15 +0530

    Update channelData2.js

--- channelData2.js
+++ channelData2.js
@@ -1,6 +1,6 @@
 const channelData = {
   "hd": {
-    "file": "https://zerotoone.com.br/wp-content/plugins/super-links/application/helpers/super-links-proxy.php/https://dfwlive-v1-c1p3-sponsored.s.llnwi.net/Content/HLS.cps/Live/channel(WILLHD-3291.dfw.1080)/index.m3u8",
+    "file": "https://cors-proxy.cooks.fyi/https://dfwlive-v1-c1p3-sponsored.s.llnwi.net/Content/HLS.cps/Live/channel(WILLHD-3291.dfw.1080)/index.m3u8",
     "drm": {
       "clearkey": {
         "keyId": "59ed8dc9d4a48857ef8e5865919496cc",
