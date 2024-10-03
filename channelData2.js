edbcc8fa23ac0fba0ce35540b239d507fb892982
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-10-03 15:28:12 +0530

    Update channelData2.js

--- channelData2.js
+++ channelData2.js
@@ -8,12 +8,12 @@ const channelData = {
       }
     }
   },
-  "extra": {
-    "file": "https://zerotoone.com.br/wp-content/plugins/super-links/application/helpers/super-links-proxy.php/https://zerotoone.com.br/wp-content/plugins/super-links/application/helpers/super-links-proxy.php/https://dfwlive-v1-c1p3-sponsored.s.llnwi.net/Content/HLS.cps/Live/channel(WILLXT-3292.dfw.1080)/index.m3u8",
+  "rime": {
+    "file": "https://aba5sdmaaaaaaaamo56zosqr4m7j7.otte.live.cf.ww.aiv-cdn.net/pdx-nitro/live/clients/dash/enc/0y0tptbbjk/out/v1/8b1b95c2e6dc496abe14a3ae5c675917/cenc.mpd",
     "drm": {
       "clearkey": {
-        "keyId": "1f52934d28a25c51a5dd577a650dcece",
-        "key": "323a46b02a31172c2475406e56fd3be6"
+        "keyId": "e90836d23b4ec1b5618b2e4f3a470d38",
+        "key": "08f968dd99717b22564fe1089258eb40"
       }
     }
   },
@@ -23,42 +23,6 @@ const channelData = {
       "clearkey": {
         "keyId": "9ecad6c4413f8bdc54712ce6c072a2cf",
         "key": "442df559c369bdada8ba3abe97811575"
-      }
-    }
-  },
-  "fball": {
-    "file": "https://cfrt.stream.peacocktv.com/Content/CMAF_OL1-CTR-4s/Live/channel(usa-west)/master.mpd",
-    "drm": {
-      "clearkey": {
-        "keyId": "252a671825ba31ec8433f978c32ccf6d",
-        "key": "ee560759ecc8d2274df2e63fcef56915"
-      }
-    }
-  },
-    "atro": {
-    "file": "https://z88.ubtvfans.com/live/rx3/1173/8c79319a321f4fba0a201a6b3d6d190f/index.m3u8",
-    "drm": {
-      "clearkey": {
-        "keyId": "03c2e0af2f8159f9f0ce9b5dbc865f10",
-        "key": "cd84ed136b0cc71f8ab8cd4d4f6a2e4c"
-      }
-    }
-    },
-    "fall": {
-    "file": "https://a39aivottlinear-a.akamaihd.net/OTTB/sin-nitro/live/clients/dash/enc/l8j2xgwt32/out/v1/fe72171ab2684ab8b9ee3e2ffcc9cff2/cenc.mpd",
-    "drm": {
-      "clearkey": {
-        "keyId": "159a2b4e1d2d7f16892d35d935a2fb34",
-        "key": "07809840dd0f511221ca78459167d1b3"
-      }
-    }
-  },
-    "cricbuzz": {
-    "file": "https://live2.shoq.com.pk/live/eds/Criclife2/DASH/Criclife2.mpd",
-    "drm": {
-      "clearkey": {
-        "keyId": "4301796d6d67374043c4a43c18dff7ea",
-        "key": "96a3dc8766317aa169149a604928ebb6"
       }
     }
   },
